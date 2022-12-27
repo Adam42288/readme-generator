69b4be9fdd393bc3cb152176bc5ec09e898327c1
Adam <asantillana@apple.com>
2022-12-27 11:52:33 -0600

    fixed badge URL

--- Develop/index.js
+++ Develop/index.js
@@ -18,7 +18,7 @@ const questions = [
         type: "input",
     },
     {
-        name: "installation",
+        name: "installations",
         message: "Enter the installation instructions for your project",
         type: "input",
     },
@@ -41,7 +41,7 @@ const questions = [
         name: "license",
         type: "list",
         message: "Choose a license for your project",
-        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'BSD 3'],
     },
     {
         name: "username",
@@ -58,32 +58,25 @@ const questions = [
 
 
 // TODO: Create a function to write README file
-function generateMD(answers){
+function createReadMe(answers){
  
     let badge = "";
     if(answers.license == "MIT"){
-        badge = "![License](https://img.shields.io/static/v1?label=License&message=MIT&color=blueviolet&style=plastic)"
-    }else if (answers.license == "APACHE 2.0"){
-        badge = "![License](https://img.shields.io/static/v1?label=License&message=APACHE2.0&color=blueviolet&style=plastic)"
-    }else if (answers.license == "GPL 3.0"){
-    }else if (answers.license == "APACHE 2.0"){
-        badge = "![License](https://img.shields.io/static/v1?label=License&message=APACHE2.0&color=blueviolet&style=plastic)"
-    }else if (answers.license == "GPL 3.0"){
-    }else if (answers.license == "APACHE 2.0"){
-        badge = "![License](https://img.shields.io/static/v1?label=License&message=APACHE2.0&color=blueviolet&style=plastic)"
-    }else if (answers.license == "GPL 3.0"){
-    }else if (answers.license == "APACHE 2.0"){
-        badge = "![License](https://img.shields.io/static/v1?label=License&message=APACHE2.0&color=blueviolet&style=plastic)"
-    }else if (answers.license == "GPL 3.0"){
-    }else if (answers.license == "APACHE 2.0"){
-        badge = "![License](https://img.shields.io/static/v1?label=License&message=APACHE2.0&color=blueviolet&style=plastic)"
-    }else if (answers.license == "GPL 3.0"){
-    }else if (answers.license == "APACHE 2.0"){
-        badge = "![License](https://img.shields.io/static/v1?label=License&message=APACHE2.0&color=blueviolet&style=plastic)"
-    }else if (answers.license == "GPL 3.0"){
-        badge = "![License](https://img.shields.io/static/v1?label=License&message=GPL3.0&color=blueviolet&style=plastic)"
-    }else if (answers.license == "BSD 3"){
-        badge = "![License](https://img.shields.io/static/v1?label=License&message=BSD3&color=blueviolet&style=plastic)"
+        badge = "![License](https://img.shields.io/static/v1?label=License&message=MIT&color=blue&style=plastic)"
+    } else if (answers.license == "AGPLv3") {
+        badge = "![License](https://img.shields.io/static/v1?label=License&message=AGPLv3&color=blue&style=plastic)"
+    } else if (answers.license == "Boost Software License 1.0") {
+        badge = "![License](https://img.shields.io/static/v1?label=License&message=BOOSTv1&color=blue&style=plastic)"
+    } else if (answers.license == "LGPLv3") {
+        badge = "![License](https://img.shields.io/static/v1?label=License&message=LGPLv3&color=blue&style=plastic)"
+    } else if (answers.license == "Mozilla Public License 2.0") {
+        badge = "![License](https://img.shields.io/static/v1?label=License&message=MOZILLAv2&color=blue&style=plastic)"
+    } else if (answers.license == "APACHE 2.0"){
+        badge = "![License](https://img.shields.io/static/v1?label=License&message=APACHE2.0&color=blue&style=plastic)"
+    } else if (answers.license == "GPL 3.0"){
+        badge = "![License](https://img.shields.io/static/v1?label=License&message=GPL3.0&color=blue&style=plastic)"
+    } else if (answers.license == "BSD 3"){
+        badge = "![License](https://img.shields.io/static/v1?label=License&message=BSD3&color=blue&style=plastic)"
     }
     
 return`# ${answers.title}  
@@ -105,30 +98,29 @@ ${answers.usage}
 This project is licensed under:
 ${answers.license}
 ### Contributing:
-${answers.contribute}
+${answers.contribution}
 ### Tests:
 In order to test open the console and run the following:
 \`\`\`${answers.tests}\`\`\`
 ### Questions:
 If you have any questions contact me on [GitHub](https://github.com/${answers.username}) or contact 
-${answers.author} at ${answers.email}  
+me at ${answers.email}  
  `
 }
 
 inquirer.prompt(questions)
-    .then(function (answers) {
-    console.log(answers.title);
-    console.log(answers.description);
-});
+.then((answers) => writeToFile('createReadMe.md', createReadMe(answers)))
+    .then(() => console.log('Successfully wrote to index.html'))
+    .catch((err) => console.error(err));
 
-function writeToFile(fileName, answers) {
-    fs.writeFile(fileName, answers, err => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("Success!")
-    });
-}
+// writeToFile(fileName, answers) {
+//     fs.writeFile(fileName, answers, err => {
+//         if (err) {
+//             return console.log(err);
+//         }
+//         console.log("Success!")
+//     });
+// }
 
 // TODO: Create a function to initialize app
 function init() {}
