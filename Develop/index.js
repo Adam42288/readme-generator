9c1b6b29147cbaca2ab3c4ffce9e206f102922cf
Adam <asantillana@apple.com>
2022-12-27 10:40:06 -0600

    Added initial 2 questions to prompt for testing purposes

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,20 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name: "title",
+        message: "Enter the title of your project",
+        type: "input",
+    },
+    {
+        name: "description",
+        message: "Enter a description of your project",
+        type: "input",
+    }
+];
+
+inquirer.createPromptModule(question);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
